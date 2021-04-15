c4030fb30e2fc4bbd900808c37f57fc35c750b7b
hack_dev <manueldevmx@gmail.com>
2021-04-15 13:27:54 -0500

    Update Login.tsx
    Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,96 @@
-import React, { useReducer } from 'react'
 
+import React, { useReducer, useEffect } from 'react'
+
+
+interface AuthState {
+    validando: boolean;
+    token: string | null,
+    username: string,
+    nombre: string
+}
+
+
+const initialState: AuthState = {
+    validando: true,
+    token: null,
+    username:'',
+    nombre: ''
+}
+
+type LoginPayload = {
+    username: string,
+    nombre: string
+}
+
+type AuthAction = 
+   | { type: 'logout' }
+   | { type: 'login', payload: LoginPayload }
+
+
+const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {
+
+    switch ( action.type ) {
+        case 'logout':
+            return {
+                validando: false,
+                token: null,
+                nombre: '',
+                username: ''
+            }
+
+        case 'login':
+            const { nombre, username } = action.payload;
+            return{
+               validando: false,
+               token: 'SHA-256',
+               nombre,
+               username 
+            }
+    
+        default:
+            return state;
+    }
+
+
+}
 
 export const Login = () => {
 
-    //Aca va toda la logica
+    const [{validando, token, nombre}, dispatch] = useReducer ( authReducer, initialState);
 
-   // const [state, dispatch] = useReducer(reducer, initialState)
+    useEffect(() => {
+      setTimeout (() => {
+          dispatch({type: 'logout'});
+      }, 1000)
+    }, []);
+
+
+    const login = () => {
+        dispatch({ 
+            type: 'login', 
+            payload: {
+                nombre: 'Manuel',
+                username: 'Hackguar'
+                   }
+            })
+    }
+
+    const logout = () => {
+        dispatch({ type: 'logout'});
+    }
+    
+
+
+    if ( validando) {
+        return(
+            <>
+                <h3>Iniciar Sesion</h3>
+                <div className="alert alert-info">
+                    Validando . . . 
+                </div>
+            </>
+        )
+    }
 
     return (
 
@@ -13,31 +98,32 @@ export const Login = () => {
         <>
             <h3>Inicio de Sesion</h3>
 
-            <div className="alert alert-info">
-                validando
-            </div>
+            {
+                (token)
+                ? <div className="alert alert-success"> Autenticado como: {nombre} </div>
+                : <div className="alert alert-danger"> No Autenticado </div>
+            }
 
-            <div className="alert alert-danger">
-                No autenticado
-            </div>
-
-            <div className="alert alert-success">
-             Autenticado
-            </div>
-
-
-            <button 
-                className="btn btn-success"
+            {
+                (token)
+                ? (
+                <button 
+                     className="btn btn-danger"
+                     onClick={ logout }
                 >
-                Iniciar Sesion
-            </button>
-
-            <button 
-                className="btn btn-warning"
-                >
-                Cerrar Sesion
-            </button>
-
+                     Cerrar Sesion
+                </button>
+                )
+                :
+                (
+                    <button 
+                    className="btn btn-success"
+                    onClick={ login }
+                    >
+                    Iniciar Sesion
+                </button>
+                )
+            }   
         </>
     )
 }
